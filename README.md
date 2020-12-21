f9a78593c5c0206409318ea1205b2c486826d8d0
killagu <killa123@126.com>
2020-12-21 21:38:23 +0800

    add show case

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # npminstall_webpack
+
+# how to reproduce
+
+```shell
+npm i -g cnpm
+cnpm i
+npm run build
+```
---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  entry: {
+    main: "./src/index",
+  },
+  mode: "development",
+  cache: {
+    type: 'filesystem',
+  },
+};
